cc0e927f8216c38323cb41fcd5b0df431157efa3
rodrigo <rodrigog3wconcept@gmail.com>
2024-09-23 17:00:06 -0300

    Atualização 23/09

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,12 @@
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import  render, redirect, get_object_or_404
 from django.urls import reverse
 
 from contact.forms import ContactForm
 from contact.models import Contact
 
-
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -17,7 +18,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -37,8 +40,11 @@ def create(request):
         context
     )   
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
@@ -70,10 +76,11 @@ def update(request, contact_id):
         context
     )   
 
-
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
-
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
